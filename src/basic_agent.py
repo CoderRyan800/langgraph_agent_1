603cabe4a7888dfd76b2da1cf3ce4fc250840476
CoderRyan800 <rmukai68@gmail.com>
2025-01-12 09:12:17 -0800

    Now object oriented

--- src/basic_agent.py
+++ src/basic_agent.py
@@ -5,91 +5,6 @@ from langchain_core.messages import SystemMessage, RemoveMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import MessagesState, StateGraph, START, END
 
-memory = MemorySaver()
-
-
-# We will add a `summary` attribute (in addition to `messages` key,
-# which MessagesState already has)
-class State(MessagesState):
-    summary: str
-
-
-# We will use this model for both the conversation and the summarization
-model = ChatOpenAI(model="gpt-4", temperature=0)
-
-
-# Define the logic to call the model
-def call_model(state: State):
-    # If a summary exists, we add this in as a system message
-    summary = state.get("summary", "")
-    if summary:
-        system_message = f"Summary of conversation earlier: {summary}"
-        messages = [SystemMessage(content=system_message)] + state["messages"]
-    else:
-        messages = state["messages"]
-    response = model.invoke(messages)
-    # We return a list, because this will get added to the existing list
-    return {"messages": [response]}
-
-
-# We now define the logic for determining whether to end or summarize the conversation
-def should_continue(state: State) -> Literal["summarize_conversation", END]:
-    """Return the next node to execute."""
-    messages = state["messages"]
-    # If there are more than six messages, then we summarize the conversation
-    if len(messages) > 6:
-        return "summarize_conversation"
-    # Otherwise we can just end
-    return END
-
-
-def summarize_conversation(state: State):
-    # First, we summarize the conversation
-    summary = state.get("summary", "")
-    if summary:
-        # If a summary already exists, we use a different system prompt
-        # to summarize it than if one didn't
-        summary_message = (
-            f"This is summary of the conversation to date: {summary}\n\n"
-            "Extend the summary by taking into account the new messages above:"
-        )
-    else:
-        summary_message = "Create a summary of the conversation above:"
-
-    messages = state["messages"] + [HumanMessage(content=summary_message)]
-    response = model.invoke(messages)
-    # We now need to delete messages that we no longer want to show up
-    # I will delete all but the last two messages, but you can change this
-    delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
-    return {"summary": response.content, "messages": delete_messages}
-
-
-# Define a new graph
-workflow = StateGraph(State)
-
-# Define the conversation node and the summarize node
-workflow.add_node("conversation", call_model)
-workflow.add_node(summarize_conversation)
-
-# Set the entrypoint as conversation
-workflow.add_edge(START, "conversation")
-
-# We now add a conditional edge
-workflow.add_conditional_edges(
-    # First, we define the start node. We use `conversation`.
-    # This means these are the edges taken after the `conversation` node is called.
-    "conversation",
-    # Next, we pass in the function that will determine which node is called next.
-    should_continue,
-)
-
-# We now add a normal edge from `summarize_conversation` to END.
-# This means that after `summarize_conversation` is called, we end.
-workflow.add_edge("summarize_conversation", END)
-
-# Finally, we compile it!
-app = workflow.compile(checkpointer=memory)
-
 def print_update(update):
     for k, v in update.items():
         for m in v["messages"]:
@@ -98,20 +13,86 @@ def print_update(update):
             print(v["summary"])
 
 from langchain_core.messages import HumanMessage
+# We will add a `summary` attribute (in addition to `messages` key,
+# which MessagesState already has)
+class State(MessagesState):
+    summary: str
 
-config = {"configurable": {"thread_id": "4"}}
-input_message = HumanMessage(content="hi! I'm bob")
-input_message.pretty_print()
-for event in app.stream({"messages": [input_message]}, config, stream_mode="updates"):
+class BasicAgent:
+    def __init__(self, model_name="gpt-4", temperature=0, messages_before_summary=6):
+        self.memory = MemorySaver()
+        self.model = ChatOpenAI(model=model_name, temperature=temperature)
+        self.messages_before_summary = messages_before_summary
+        self.app = self._create_workflow()
+
+    def _call_model(self, state: State):
+        summary = state.get("summary", "")
+        if summary:
+            system_message = f"Summary of conversation earlier: {summary}"
+            messages = [SystemMessage(content=system_message)] + state["messages"]
+        else:
+            messages = state["messages"]
+        response = self.model.invoke(messages)
+        return {"messages": [response]}
+
+    def _should_continue(self, state: State) -> Literal["summarize_conversation", END]:
+        """Return the next node to execute."""
+        messages = state["messages"]
+        if len(messages) > self.messages_before_summary:
+            return "summarize_conversation"
+        return END
+
+    def _summarize_conversation(self, state: State):
+        summary = state.get("summary", "")
+        if summary:
+            summary_message = (
+                f"This is summary of the conversation to date: {summary}\n\n"
+                "Extend the summary by taking into account the new messages above:"
+            )
+        else:
+            summary_message = "Create a summary of the conversation above:"
+
+        messages = state["messages"] + [HumanMessage(content=summary_message)]
+        response = self.model.invoke(messages)
+        delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
+        return {"summary": response.content, "messages": delete_messages}
+
+    def _create_workflow(self):
+        # Define a new graph
+        workflow = StateGraph(State)
+
+        # Define the conversation node and the summarize node
+        workflow.add_node("conversation", self._call_model)
+        workflow.add_node("summarize_conversation", self._summarize_conversation)
+
+        # Set the entrypoint as conversation
+        workflow.add_edge(START, "conversation")
+
+        # Add conditional edges
+        workflow.add_conditional_edges(
+            "conversation",
+            self._should_continue,
+        )
+
+        # Add edge from summarize_conversation to END
+        workflow.add_edge("summarize_conversation", END)
+
+        return workflow.compile(checkpointer=self.memory)
+
+    def chat(self, message: str, config: dict = None):
+        if config is None:
+            config = {"configurable": {"thread_id": "default"}}
+        
+        input_message = HumanMessage(content=message)
+        return self.app.stream({"messages": [input_message]}, config, stream_mode="updates")
+
+
+agent = BasicAgent()
+
+# Chat with the agent
+for event in agent.chat("hi! I'm bob"):
     print_update(event)
 
-input_message = HumanMessage(content="what's my name?")
-input_message.pretty_print()
-for event in app.stream({"messages": [input_message]}, config, stream_mode="updates"):
-    print_update(event)
-
-input_message = HumanMessage(content="i like the celtics!")
-input_message.pretty_print()
-for event in app.stream({"messages": [input_message]}, config, stream_mode="updates"):
+for event in agent.chat("what's my name?"):
     print_update(event)
 
