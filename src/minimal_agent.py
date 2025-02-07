f7a66d9fe5f4855a85951578e80fcfe62def80d2
CoderRyan800 <rmukai68@gmail.com>
2025-02-07 13:49:57 -0800

    Minimal example with tool calls

---
+++ src/minimal_agent.py
@@ -0,0 +1,59 @@
+from langchain_core.messages import AIMessage
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import ToolNode
+
+@tool
+def get_weather(location: str):
+    """Call to get the current weather."""
+    if location.lower() in ["sf", "san francisco"]:
+        return "It's 60 degrees and foggy."
+    else:
+        return "It's 90 degrees and sunny."
+
+
+@tool
+def get_coolest_cities():
+    """Get a list of coolest cities"""
+    return "nyc, sf"
+
+tools = [get_weather, get_coolest_cities]
+tool_node = ToolNode(tools)
+
+message_with_single_tool_call = AIMessage(
+    content="",
+    tool_calls=[
+        {
+            "name": "get_weather",
+            "args": {"location": "sf"},
+            "id": "tool_call_id",
+            "type": "tool_call",
+        }
+    ],
+)
+
+print(tool_node.invoke({"messages": [message_with_single_tool_call]}))
+
+message_with_multiple_tool_calls = AIMessage(
+    content="",
+    tool_calls=[
+        {
+            "name": "get_coolest_cities",
+            "args": {},
+            "id": "tool_call_id_1",
+            "type": "tool_call",
+        },
+        {
+            "name": "get_weather",
+            "args": {"location": "sf"},
+            "id": "tool_call_id_2",
+            "type": "tool_call",
+        },
+    ],
+)
+
+print(tool_node.invoke({"messages": [message_with_multiple_tool_calls]}))
+
+model_with_tools = ChatOpenAI(model='gpt-4o', temperature=0).bind_tools(tools)
+
+print(model_with_tools.invoke("what's the weather in sf?").tool_calls)
